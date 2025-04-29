cc29e231da94b758e2ad603e3a5face4c0a64ea4
mdmrabaa <mdmrabaa@gmail.com>
2025-04-29 19:16:14 +0100

    Update user.js

--- routes/auth/user.js
+++ routes/auth/user.js
@@ -1,47 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../../models/user');
-const nodemailer = require('nodemailer');
 const multer = require('multer');
 const path = require('path');
 const bcrypt = require('bcrypt');
 const dotenv = require('dotenv');
 dotenv.config();
-
-// Set up the email transporter with Hostinger SMTP settings
-const transporter = nodemailer.createTransport({
-  host: 'smtp.hostinger.com',
-  port: 465,
-  secure: true,
-  auth: {
-    user: process.env.HOSTINGER_USER,
-    pass: process.env.HOSTINGER_PASS
-  },
-  debug: true // Enable debug output
-});
-
-
-// Function to send the confirmation email
-const sendConfirmationEmail = (userEmail) => {
-  const confirmationUrl = `https://ubc-back.onrender.com/confirm/confirm/${userEmail}`;
-
-  const mailOptions = {
-    from: process.env.HOSTINGER_USER,
-    to: userEmail,
-    subject: 'Please confirm your account',
-    html: `<h1>Email Confirmation</h1>
-           <p>Thank you for registering. Please confirm your email by clicking on the following link:</p>
-           <a href="${confirmationUrl}">Confirm Email</a>`
-  };
-
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.error('Error sending confirmation email:', error);
-    } else {
-      console.log('Confirmation email sent:', info.response);
-    }
-  });
-};
 
 const storage = multer.diskStorage({
   destination: './uploads/',
@@ -80,9 +44,6 @@ router.post('/', upload.single('photo'), async (req, res) => {
     });
 
     await newUser.save();
-
-    // Send the confirmation email
-    sendConfirmationEmail(email);
 
     res.status(200).json({
       id: newUser._id,
